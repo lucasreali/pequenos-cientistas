1ba4242fcaa39ebc8d171d27efb5d7743765db41
Lucas Reali <lucasrealioficial@gmail.com>
2024-10-09 16:52:06 -0300

    Update the contato page

--- router/routes.php
+++ router/routes.php
@@ -4,7 +4,7 @@ $routes = [
     '/' => 'pages/home.php',
     '/login' => 'pages/login.php',
     '/sobre' => 'pages/sobre.php',
-    '/contato' => 'pages/contato.php',
+    '/contato' => 'pages/contato.html',
     '/quemevoce' => 'pages/usertype-page.html',
     '/singup' => 'pages/singup.php'
 ];
