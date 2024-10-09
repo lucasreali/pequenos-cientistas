57014f4675babf6d9c004c45b251618fc31b6f87
lucasreali <lucasreali@github.com>
2024-10-09 10:38:17 -0300

    add singup.php

---
+++ pages/singup.php
@@ -0,0 +1,53 @@
+<?php
+$usertype = $_POST['usertype'];
+
+
+?><!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="../assets/images/logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="../assets/styles/style.css">
+    <link rel="stylesheet" href="../assets/styles/login.css">
+    <title>Login</title>
+</head>
+<body>
+<section>
+    <div class="form-login">
+        <form name="form" action="../actions/login.php" method="post">
+            <div class="box-size">
+                <a href="/quemevoce"><img src="../assets/images/arrow-left-circle.svg" alt="voltar para a pagina inicial"></a>
+            </div>
+                <div class="header">
+                    <img src="../assets/images/<?= $usertype ?>-ico.svg" alt="<?= $usertype ?> icone">
+                    <h3>Obrigado por querer fazer parte dos</h3>
+                    <h1>Pequenos Cientistas!</h1>
+                </div>
+
+            <div class="input-wrapper">
+                <label for="email">E-mail:</label>
+                <input type="email" name="email" placeholder="Digite aqui">
+            </div>
+
+            <div class="input-wrapper">
+                <label for="password">Password:</label>
+                <input type="password" name="password" placeholder="Digite aqui">
+            </div>
+
+            <p class="fgt-passw"><a href="/">Esqueci minha senha</a></p>
+
+            <button type="submit">Proximo</button>
+
+            <p>Ja possui conto? <a href="/login">Clique aqui</a></p>
+
+        </form>
+    </div>
+    <div class="img-login">
+        <img src="../assets/images/logo-completa.svg" alt="">
+    </div>
+</section>
+</body>
+</html>
--- router/routes.php
+++ router/routes.php
@@ -5,5 +5,6 @@ $routes = [
     '/login' => 'pages/login.php',
     '/sobre' => 'pages/sobre.php',
     '/contato' => 'pages/contato.php',
-    '/quemevoce' => 'pages/usertype.html'
+    '/quemevoce' => 'pages/usertype-page.html',
+    '/singup' => 'pages/singup.php'
 ];
