8c879383ab08540dfe040ed72491405f01d7f356
lucasreali <lucasreali@github.com>
2024-10-25 16:38:53 -0300

    Evitando erros com o composer

---
+++ pages/responsavel/responsavel.php
@@ -0,0 +1,52 @@
+<?php
+
+include 'models/ResponsavelModel.php';
+session_start();
+
+if ($_SESSION['user_type'] != 'responsavel' || !isset($_SESSION['user_type'])) {
+    header('Location: /');
+    exit();
+}
+
+$ResponsavelModel = new ResponsavelModel();
+
+$dependentes = $ResponsavelModel->getDependentes();
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/responsavel.css">
+    <link rel="stylesheet" href="assets/styles/imports.css">
+    <title>Responsavel</title>
+</head>
+<body>
+    
+<header>
+        <nav>
+            <a href="/admin"><img src="assets/images/logo.svg" alt=""></a>
+
+            <ul class="container">
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+                <li><a href="">Lorem</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <section>
+
+        <div>
+
+            
+
+        </div>
+
+    </section>
+
+</body>
+</html>
