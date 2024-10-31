6b5175d9b1d27f03efbccc0429bd1a243770cc47
oGabrie1 <gabrielconti5563@gmail.com>
2024-10-31 10:02:27 -0300

    Criação página sobre e estilização

---
+++ pages/geral/sobre.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/pequenos-cientistas/assets/images/logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="/pequenos-cientistas/assets/styles/imports.css">
+    <link rel="stylesheet" href="/pequenos-cientistas/assets/styles/style.css">
+    <link rel="stylesheet" href="/pequenos-cientistas/assets/styles/sobre.css">
+
+    <title>Pequenos cientistas</title>
+</head>
+<header>
+        <nav>
+            <a href="/"><img src="/pequenos-cientistas/assets/images/logo.svg" alt=""></a>
+            <div class="menu-wrapper container">
+                <ul>
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                    <li><a href="/sobre">Sobre nós</a></li>
+                </ul>
+
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+    </header>
+
+    <section>
+
+        <div class="left_page">
+            <h1>Nossa Missão</h1>
+            <p>
+            No projeto Pequenos Cientistas, queremos inspirar a curiosidade natural de crianças e jovens pelo conhecimento científico, oferecendo um ambiente acolhedor e acessível para que explorem desde conceitos básicos, como átomos, até mistérios como buracos negros. A plataforma busca preencher a lacuna deixada pelo ensino tradicional em áreas como astronomia, incentivando uma visão investigativa e apaixonada pelo mundo. Valorizamos cada usuário como parte essencial do projeto e oferecemos uma experiência interativa com tarefas e pontos de experiência (XP), cultivando uma conexão significativa com a ciência e incentivando o aprendizado contínuo.
+            </p>
+        </div>
+        <div class="right_page">
+            <h1>
+                Fundadores
+            </h1>
+            <ul>
+                <li>
+                    <a href="">
+                    <img>
+                    <h3>Enzo Alves</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                    <img>
+                    <h3>Lucas Reali</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                    <img>
+                    <h3>Thiago Akimoto</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                    <img>
+                    <h3>Gabriel Conti</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                    <img>
+                    <h3>Leonardo Ceccon</h3>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                    <img>
+                    <h3>Vitor Lisboa</h3>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </section>
+    
+</body>
+</html>
